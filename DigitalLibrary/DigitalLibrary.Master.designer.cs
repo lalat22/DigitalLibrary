a961a5383e89626d6e1476a02365a14a4e89aa8f
ShikhaGisfy <113886365+ShikhaGisfy@users.noreply.github.com>
2023-01-26 17:39:35 +0530

    Mater page integration

--- DigitalLibrary/DigitalLibrary.Master.designer.cs
+++ DigitalLibrary/DigitalLibrary.Master.designer.cs
@@ -11,8 +11,17 @@ namespace DigitalLibrary
 {
 
 
-    public partial class SiteMaster
+    public partial class DigitalLibrary
     {
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
         /// MainContent control.
