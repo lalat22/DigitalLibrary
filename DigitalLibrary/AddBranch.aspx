08376c967eadf5b01ae193ca49a8288bbf860e25
lalatkumar khuntia <lalatku97@gmail.com>
2023-01-28 06:42:48 +0530

    Add branch module integrtion

---
+++ DigitalLibrary/AddBranch.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/DigitalLibrary.Master" AutoEventWireup="true" CodeBehind="AddBranch.aspx.cs" Inherits="DigitalLibrary.AddBranch" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="card text-center" style="width: 95%; margin-left: -75px;">
+        <div class="card-header" style="background-color: #0b3355;">
+
+            <h6 style="color: ghostwhite;"><b>Add Branch</b></h6>
+        </div>
+        <div class="card-body" style="background-color: rgb(37, 150, 190);">
+
+            <div class="form-row" style="margin-left: 500px;">
+                <div class="form-group col-sm-3">
+                    <%--<label for="inputEmail4">Add Branch</label>--%>
+                    <h5>Branch Name  </h5>
+                    <asp:TextBox ID="txtBranchName" runat="server" class="form-control" placeholder="Branch Name"></asp:TextBox>
+                    <%--<input type="email" class="form-control" id="inputEmail4" placeholder="Branch Name">--%>
+                </div>
+
+            </div>
+            <asp:Button  ID="btnSave" runat="server" Text="Add Branch" OnClick="btnSave_Click" class="btn btn-primary" style="margin-left: -5px; margin-top: 7px;" />
+            <%--<button type="submit" class="btn btn-primary" style="margin-left: -5px; margin-top: 7px;">Add Branch</button>--%>
+            <asp:Label ID="lblMsg" runat="server"></asp:Label>
+            <div style="margin-left: 495px;margin-top: 41px;">
+                <asp:GridView ID="gvBranch" runat="server" AutoGenerateColumns="False" CellPadding="6" OnRowCommand="gvBranch_RowCommand"
+                     OnRowEditing="gvBranch_RowEditing" OnRowDeleting="gvBranch_RowDeleting">
+                    <Columns>
+                        <asp:TemplateField HeaderText="Branch Name">
+                            <ItemTemplate>
+                                <asp:Label ID="lblBranch" runat="server" Text='<%#Eval("BranchName") %>'></asp:Label>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Actions">
+                            <ItemTemplate>
+                                <asp:LinkButton ID="lnkEdit" runat="server" CommandName="edit" CommandArgument='<%#Eval("BranchId") %>'>Edit</asp:LinkButton>
+                                <asp:LinkButton ID="lnkDelete" runat="server" CommandName="delete" CommandArgument='<%#Eval("BranchId") %>'>Delete</asp:LinkButton>
+                            </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+
+                    <HeaderStyle BackColor="#663300" ForeColor="#ffffff" />
+                    <RowStyle BackColor="#e7ceb6" />
+                </asp:GridView>
+
+            </div>
+            
+
+        </div>
+        <div class="card-footer text-muted">
+            2 days ago
+        </div>
+    </div>
+</asp:Content>
---
+++ DigitalLibrary/Service/BranchService.cs
@@ -0,0 +1,166 @@
+﻿using DigitalLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DigitalLibrary.Service
+{
+    public class BranchService
+    {
+        string ConnectionString = string.Empty;
+        private SqlCommand _sqlCommand;
+        private SqlDataAdapter _sqlDataAdapter;
+        private DataSet _dtSet;
+
+        public BranchService()
+        {
+            ConnectionString = ConfigurationManager.ConnectionStrings["myDbConnection"].ConnectionString;
+        }
+        public void CreateConnection()
+        {
+            SqlConnection _sqlConnection = new SqlConnection(ConnectionString);
+            _sqlCommand = new SqlCommand();
+            _sqlCommand.Connection = _sqlConnection;
+        }
+        public void OpenConnection()
+        {
+            _sqlCommand.Connection.Open();
+        }
+        public void CloseConnection()
+        {
+            _sqlCommand.Connection.Close();
+        }
+        public void DisposeConnection()
+        {
+            _sqlCommand.Connection.Dispose();
+        }
+
+        #region GetAllBranch
+        public List<BranchModel> GetAllBranch()
+        {
+            List<BranchModel> lstBranch = null;
+            BranchModel branchModel = null;
+            try
+            {
+                CreateConnection();
+                OpenConnection();
+                _sqlCommand.CommandText = "[dbo].[BRANCH_SELECT]";
+                _sqlCommand.CommandType = CommandType.StoredProcedure;
+                _sqlDataAdapter = new SqlDataAdapter(_sqlCommand);
+                _dtSet = new DataSet();
+                _sqlDataAdapter.Fill(_dtSet);
+                DataTable dt = new DataTable();
+                dt = _dtSet.Tables[0];
+
+                if (dt.Rows.Count > 0)
+                {
+                    lstBranch = new List<BranchModel>();
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        BranchModel obj  = new BranchModel();
+                        obj.BranchId = Convert.ToInt32(dt.Rows[i]["BranchId"]);
+                        obj.BranchName = dt.Rows[i]["BranchName"].ToString();
+
+                        lstBranch.Add(obj);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+
+            }
+            finally
+            {
+                CloseConnection();
+                DisposeConnection();
+                branchModel = null;
+            }
+            return lstBranch;
+        }
+        #endregion
+
+        #region GetBranchById
+        public BranchModel GetBranchById(int branchId)
+        {
+            BranchModel objBranchModel = null;
+            try
+            {
+                CreateConnection();
+                OpenConnection();
+                _sqlCommand.CommandText = "BRANCH_SELECT_BY_BID";
+                _sqlCommand.CommandType = CommandType.StoredProcedure;
+                _sqlCommand.Parameters.AddWithValue("@BranchId", branchId);
+
+                _sqlDataAdapter = new SqlDataAdapter(_sqlCommand);
+                _dtSet = new DataSet();
+                _sqlDataAdapter.Fill(_dtSet);
+                if ((_dtSet != null) && (_dtSet.Tables[0].Rows.Count > 0))
+                {
+                    objBranchModel = new BranchModel();
+                    objBranchModel.BranchName = _dtSet.Tables[0].Rows[0]["BranchName"].ToString();
+                    objBranchModel.BranchId = Convert.ToInt32(_dtSet.Tables[0].Rows[0]["BranchId"].ToString());
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+            return objBranchModel;
+        }
+        #endregion
+
+        #region InsertBranch
+        public int InsertBranch(string branchName)
+        {
+            int i = 0;
+            try
+            {
+                CreateConnection();
+                OpenConnection();
+                _sqlCommand.CommandText = "BRANCH_INSERT";
+                _sqlCommand.CommandType = CommandType.StoredProcedure;
+                _sqlCommand.Parameters.AddWithValue("@BranchName", branchName);
+                i = Convert.ToInt32(_sqlCommand.ExecuteNonQuery());
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return i;
+        }
+
+        #endregion
+
+        #region DeleteBranch
+        public int DeleteBranchById(int branchId)
+        {
+            int i = 0;
+            try
+            {
+                CreateConnection();
+                OpenConnection();
+                _sqlCommand.CommandText = "BRANCH_DELETE";
+                _sqlCommand.CommandType = CommandType.StoredProcedure;
+                _sqlCommand.Parameters.AddWithValue("@BranchId", branchId);
+                i = Convert.ToInt32(_sqlCommand.ExecuteNonQuery());
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return i;
+        }
+        #endregion
+    }
+}
